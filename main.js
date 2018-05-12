c952644dde0b20b97c5fe09ecd2eedfbe7733923
Spencer <spencerwilliammartin@gmail.com>
2018-05-12 13:19:25 -0700

    optimized functions and changed event listener from button click to form submit

--- main.js
+++ main.js
@@ -2,64 +2,98 @@
 
 $(document).ready(function () {
     const access_key = '0b60243c6e779ce451127759d8b9c8fe';
+    const API_root = 'https://data.fixer.io/api/';
 
-    // access object of all available currencies
+    /**
+     * accesses object of all available currencies
+     */
     $.ajax({
-        url: 'https://data.fixer.io/api/symbols' + '?access_key=' + access_key,
-        dataType: 'jsonp',
+        url: `${API_root}symbols?access_key=${access_key}`,
+        dataType: 'json',
         success: function (json) {
-            let currencyAcronyms = Object.keys(json.symbols);
-            let currencyNames = Object.values(json.symbols);
-    // render dropdown lists of available currencies       
-            function renderList(array) {
-                $(array).html('');
-                for (let i = 0; i < array.length; i++) {
-                    let dropdownOption = $("<option>", {
-                        value: array[i],
-                    }).text(array[i]);
-                    $(".populated-currency-list").append(dropdownOption);
-                }
-            }
-            renderList(currencyAcronyms);
+
+            renderList(json.symbols);
+            console.log(json.symbols);
         }
     });
-    // self-explanatory
-    function convertCurrency() {
-        // set endpoint
-        let endpoint = 'convert';
-        // define from currency, to currency, and amount
-        let from = $("#dropdown-1 option:selected").text();
-        let to = $("#dropdown-2 option:selected").text();
-        let amount = $("#conversion-amount").val();
-        //test for invalid characters
-        function testNumberInput() {
-            if (amount !== Number || !(amount > 0)) {
-                $(".conversion-results").html(`Conversion failed. Please try again, using only positive integers, and no commas or currency symbols.`)
-            }
+
+    /**
+     * renders dropdown lists of available currencies
+     */
+    function renderList(obj) {
+        for (const key in obj) {
+            let dropdownOption = $("<option>", {
+                value: `${key}`
+            }).text(`${obj[key]} (${key})`);
+            $(".populated-currency-list").append(dropdownOption);
         }
-        testNumberInput();
-        // execute the conversion using the "convert" endpoint:
-        $.ajax({
-            url: 'https://data.fixer.io/api/' + endpoint + '?access_key=' + access_key + '&from=' + from + '&to=' + to + '&amount=' + amount,
-            dataType: 'jsonp',
-            success: function (json) {
-                // access json data and render it in the DOM
-                function renderConversionResult(json) {
-                    let rate = json.info.rate;
-                    $(".conversion-results").html(`${amount} ${from} equals ${json.result} ${to} at a rate of ${rate}`)
-                }
-                renderConversionResult(json);
-            }
-        });
+    }
+
+    /**
+     * tests for invalid characters / same currency.
+     * displays message to user if test fails
+     */
+    function testInputs(amount, from, to) {
+        if (!(amount > 0)) {
+            $(".conversion-results").html('Please make sure that your input is a positive numeral, and that no special characters are used.');
+            return false
+        } else if (from == to) {
+            $(".conversion-results").html('Please make sure that the currencies are not the same');
+            return false
+        } else return true
+
+    }
+
+    /**
+     * defines values to be used in conversion
+     */
+    let from = $("#dropdown-1").val();
+    let to = $("#dropdown-2").val();
+    let amount = $("#conversion-amount").val();
+
+    /**
+     * executes API call for currency exchange
+     */
+    function convertCurrency() {
+        let trueOrFalse = testInputs(amount, from, to);
+
+        if (!trueOrFalse) {
+            return;
+        }
     };
-    // event listener for the "Convert!" button
+
+    /**
+     * execute the conversion using the "convert" endpoint, and then render it
+     */
+    $.ajax({
+        url: `${API_root}convert?access_key=${access_key}&from=${from}&to=${to}&amount=${amount}`,
+        dataType: 'json',
+        success: function (json) {
+
+            renderConversionResult(json);
+        }
+    });
+    /**
+     * renders conversion result to user including the base currency, amount to convert, and conversion result
+     */
+    function renderConversionResult(json) {
+        let rate = json.info.rate;
+        $(".conversion-results").html(`${amount} ${from} equals ${json.result} ${to} at a rate of ${rate}`)
+    }
+
+    /**
+     * listens for form submission and executes conversion 
+     */
     function handleConvertButtonClick(event) {
-        $(".convert-button").on('click', (function (event) {
+        $("#conversion-form").submit((function (event) {
             event.preventDefault();
             convertCurrency();
         }));
     }
-    // executes event listeners
+
+    /**
+     * executes event listeners
+     */
     function registerEvents() {
         handleConvertButtonClick();
     }
