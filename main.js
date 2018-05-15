204309590d06470ec4a120aa42eec9763b24ddca
Spencer <spencerwilliammartin@gmail.com>
2018-05-14 17:58:31 -0700

    Changes to number input in HTML, restructured JS and updated AJAX calls to use .done instead of .success. Added JS to adjust styles for .select2() dropdowns. Basic CSS added.

--- main.js
+++ main.js
@@ -3,24 +3,48 @@
 $(document).ready(function () {
     const access_key = '0b60243c6e779ce451127759d8b9c8fe';
     const API_root = 'https://data.fixer.io/api/';
+    let from = "",
+        to = "",
+        amount = 0;
+
+    /**
+     * ajax request to access all available currencies
+     */
+    const symbolsCall = $.ajax({
+        url: `${API_root}symbols?access_key=${access_key}`,
+        dataType: 'json'
+    });
+
+    // STYLING
+    function formatDropDowns() {
+        $('#dropdown-1').select2({
+            placeholder: 'Select a currency',
+            width: 'auto'
+        });
+        $('#dropdown-2').select2({
+            placeholder: 'Select a currency',
+            width: 'auto'
+        })
+    }
+
 
     /**
      * accesses object of all available currencies
      */
-    $.ajax({
-        url: `${API_root}symbols?access_key=${access_key}`,
-        dataType: 'json',
-        success: function (json) {
-
-            renderList(json.symbols);
-            console.log(json.symbols);
-        }
+    symbolsCall.done(function (json) {
+        renderList(json.symbols);
+    }).fail(function () {
+        $(".populated-currency-list").append(`Cannot retrieve currency list`);
+        return;
     });
 
     /**
      * renders dropdown lists of available currencies
      */
     function renderList(obj) {
+        $(".populated-currency-list").append($("<option>", {
+            class: `placeholder-option`
+        }))
         for (const key in obj) {
             let dropdownOption = $("<option>", {
                 value: `${key}`
@@ -30,61 +54,63 @@ $(document).ready(function () {
     }
 
     /**
-     * tests for invalid characters / same currency.
+     * tests if user selected same currency.
      * displays message to user if test fails
      */
-    function testInputs(amount, from, to) {
-        if (!(amount > 0)) {
-            $(".conversion-results").html('Please make sure that your input is a positive numeral, and that no special characters are used.');
-            return false
-        } else if (from == to) {
+
+    function testInputs() {
+        from = $("#dropdown-1").val();
+        to = $("#dropdown-2").val();
+
+        if (from == to) {
             $(".conversion-results").html('Please make sure that the currencies are not the same');
             return false
         } else return true
-
     }
-
-    /**
-     * defines values to be used in conversion
-     */
-    let from = $("#dropdown-1").val();
-    let to = $("#dropdown-2").val();
-    let amount = $("#conversion-amount").val();
 
     /**
      * executes API call for currency exchange
      */
-    function convertCurrency() {
-        let trueOrFalse = testInputs(amount, from, to);
 
+    function convertCurrency() {
+        let trueOrFalse = testInputs();
         if (!trueOrFalse) {
             return;
         }
+
+        from = $("#dropdown-1").val();
+        to = $("#dropdown-2").val();
+        amount = $("#conversion-amount").val();
+
+        const conversionCall = $.ajax({
+            url: `${API_root}convert?access_key=${access_key}&from=${from}&to=${to}&amount=${amount}`,
+            dataType: 'json'
+        });
+
+        /**
+         * execute the conversion using the "convert" endpoint, and then render it
+         */
+        conversionCall.done(function (json) {
+            renderConversionResult(json);
+        });
+
+
     };
 
-    /**
-     * execute the conversion using the "convert" endpoint, and then render it
-     */
-    $.ajax({
-        url: `${API_root}convert?access_key=${access_key}&from=${from}&to=${to}&amount=${amount}`,
-        dataType: 'json',
-        success: function (json) {
-
-            renderConversionResult(json);
-        }
-    });
     /**
      * renders conversion result to user including the base currency, amount to convert, and conversion result
      */
     function renderConversionResult(json) {
-        let rate = json.info.rate;
-        $(".conversion-results").html(`${amount} ${from} equals ${json.result} ${to} at a rate of ${rate}`)
+        from = $("#dropdown-1 option:selected").val();
+        to = $("#dropdown-2 option:selected").val();
+        amount = $("#conversion-amount").val();
+        $(".conversion-results").html(`${amount} ${from}s equals ${json.result} ${to}s.`)
     }
 
     /**
      * listens for form submission and executes conversion 
      */
-    function handleConvertButtonClick(event) {
+    function handleFormSubmit(event) {
         $("#conversion-form").submit((function (event) {
             event.preventDefault();
             convertCurrency();
@@ -92,10 +118,15 @@ $(document).ready(function () {
     }
 
     /**
+     * style inputs
+     */
+    formatDropDowns();
+
+    /**
      * executes event listeners
      */
     function registerEvents() {
-        handleConvertButtonClick();
+        handleFormSubmit();
     }
     registerEvents();
 });
